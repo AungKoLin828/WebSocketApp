61870262cef58cdeb9e67d92b5036f22f9f35f8d
AungKoLin828 <aungko.linn404@gmail.com>
2024-06-30 15:50:03 +0630

    Add comment

--- src/main/java/com/mm/websocket/handler/MyWebSocketHandler.java
+++ src/main/java/com/mm/websocket/handler/MyWebSocketHandler.java
@@ -7,15 +7,16 @@ import org.springframework.web.socket.handler.TextWebSocketHandler;
 
 public class MyWebSocketHandler extends TextWebSocketHandler{
 	
+	// Open the connection
 	public void afterConnectionEstablished(WebSocketSession session) throws Exception {
 		System.out.println("Connection Established!");
 	}
-	
+	// Handle messages
 	public void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
 		System.out.println("Received message: " + message.getPayload());
 		session.sendMessage(new TextMessage("Hello from server"));
 	}
-	
+	// Closed the connection
 	public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
         System.out.println("Connection closed!");
     }
